64031f4e7a67acd5c1063d551a7b2358bd7b0dc5
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-01-16 15:32:39 +0400

    master

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         CustomerValidator customer = new CustomerValidator("Murad");
-        BankAccount account = new BankAccount("4567",125.0);
+        BankAccount account = new BankAccount("4567", 125.0);
 
-        CurrencyConverter converter = new CurrencyConverter(170,Currency.USD,Currency.EUR);
+        CurrencyConverter converter = new CurrencyConverter(170, Currency.USD, Currency.EUR);
         double returned = converter.convert(170, "USD", "EUR");
         System.out.println(returned);
         System.out.println(converter);
